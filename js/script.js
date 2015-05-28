e76e26d3329e388b72d59a681f91ccc8665e1b18
Darío Javier Cravero <dario@uxtemple.com>
2015-05-28 21:45:26 +0100

    changes

--- js/script.js
+++ js/script.js
@@ -1,197 +1,212 @@
-$(document).ready(function(){
-	//quiz question "array"
-	var question1 = {
-		question: "What kind of animal lays the largest eggs?",
-		options: {
-			option1:"Ostrich", 
-			option2: "Emu", 
-			option3: "Shark", 
-			option4: "Platypus"},
-		answer: 3,
-	};
+!function() {
+  'use strict';
 
-	var question2 = {
-		question: "What do you call a group of alligators?",
-		options: {
-			option1: "A murder", 
-			option2: "A herd", 
-			option3: "A swarm", 
-			option4: "A congregation"},
-		answer: 4,
-	};
+  //quiz question "array"
+  var quiz = [{
+    question: "What kind of animal lays the largest eggs?",
+    choices: [
+      "Ostrich", 
+      "Emu", 
+      "Shark", 
+      "Platypus"
+    ],
+    answer: 2
+  }, {
+    question: "What do you call a group of alligators?",
+    choices: [
+      "A murder", 
+      "A herd", 
+      "A swarm", 
+      "A congregation"
+    ],
+    answer: 3
+  }, {
+    question: "What is a rhino's horn made of?",
+    choices: [
+      "Cartilage", 
+      "Hair", 
+      "Bone", 
+      "Skin"
+    ],
+    answer: 1
+  }, {
+    question: "What is a baby turkey called?",
+    choices: [
+      "Poult", 
+      "Kip", 
+      "Fledgling", 
+      "Chick"
+    ],
+    answer: 0
+  }, {
+    question: "How long can a termite queen live for?",
+    choices: [
+      "10 days", 
+      "6 months", 
+      "5 years", 
+      "50 years"
+    ],
+    correct: 3
+  }, {
+    question: "Owls are most closely related to what other birds?",
+    choices: [
+      "Hummingbirds", 
+      "Hawks", 
+      "Egrets", 
+      "Penguins"
+    ],
+    correct: 3
+  }];
 
-	var question3= {
-		question: "What is a rhino's horn made of?",
-		options: {
-			option1: "Cartilage", 
-			option2: "Hair", 
-			option3: "Bone", 
-			option4: "Skin"},
-		answer: 2,
-	}; 
+  var currentQuestionIndex = 0;
 
-	var question4 = {
-		question: "What is a baby turkey called?",
-		options: {
-			option1= "Poult", 
-			option2= "Kip", 
-			option3= "Fledgling", 
-			option4= "Chick"},
-		answer: 1,
-	}; 
+  function start() {
+    currentQuestionIndex = 0;
+    show();
+  }
+  document.getElementById('start').addEventListener('click', start);
 
-	var question5 = {
-		question: "How long can a termite queen live for?",
-		choices: {
-			choice1: "10 days", 
-			choice2: "6 months", 
-			choice3: "5 years", 
-			choice4: "50 years"},
-		correct: 4,
-	}; 
+  function createChoiceElement(choiceIndex, choiceLabel) {
+    // This isn't the best way to create DOM elements!
+    return (
+      '<input type="radio" name="option" class="option" value="' + choiceIndex + '">' +
+      '<span class="option">' + choiceLabel + '</span> <br>'
+    );
+  }
 
+  function show() {
+    var question = quiz[currentQuestionIndex];
 
-	var question6 = {
-		question: "Owls are most closely related to what other birds?",
-		choices: {
-			choice1: "Hummingbirds", 
-			choice2: "Hawks", 
-			choice3: "Egrets", 
-			choice4: "Penguins"},
-		correct: 4,
-	}; 
+    var choices = question.choices.map(function(choice, index) {
+      return createChoiceElement(index, choice);
+    });
 
-	//array to run functions
-	var quiz = {
-		1: quesion1,
-		2: question2,
-		3: question3, 
-		4: question4, 
-		5: question5,
-		6: question6, 
-	};
+    document.getElementById('choices').innerHTML = choices.join('');
+  }
 
-	//functions!
+  function check() {
 
-	function newQuestion(question){
-		//default options to incorrect
-		$('.option').addClass('incorrect').css('cursor', 'pointer');
-
-		//set question
-		$('.question').text(question.quote);
-
-		//set options
-		$('.option.1').attr('src', question.options.option1);
-		$('.option.2').attr('src', question.options.option2);
-		$('.option.3').attr('src', question.options.option3);
-		$('.option.4').attr('src', question.options.option4);
-
-		//add correct flag to right answer
-		$('.option.' + question.answer).attr('id', 'correct').removeClass('incorrect'); 
-
-	};
-
-	//evaluate if correct answer is selected
-	function evaluateGuess(question) {
-		var submitted=false; 
-		//correct answers
-		$('#correct').bind('click', function() {
-			if submitted== false){
-			$('.submitButton').show();
-			correct=true;
-			$('.option').removeClass('selected'); 
-			$(this).addClass('selected'); 
-
-		};
-
-		});
-
-		//incorrect answers
-		$('.incorrect').bind('click', function(){ 
-			if(submitted==false){
-				$('.submitButton').show(); 
-				correct = false;
-				$('.option').removeClass('selected'); 
-				$(this).addClass('selected'); 
-			};
-		});
-
-		//more with selectedd item
-		$('.submitButton').click(function(){
-			submitted=true;
-			answerClicked = document.getElementsByClassName('selected')[0].getAttribute('src');
-			//correct answer
-			if (correct==true) {
-				answerCounter = document.getElementById('correctCount'); 
-				correctCount.innerHTML++; 
-				$('.feedBackText').show().text("Correct!"); 
-				$('.nextButton').show(); 
-				$('.submitButton').hide(); 
-			}
-
-			//incorrect answer
-			else if (correct==false) {
-				$('.feedBackText').show().text("Incorrect!"); 
-				$('.nextButton').show(); 
-				$('.submitButton').hide(); 
-			}
-			
-		});
-
-		//for all the quesiton within the quiz do this
-		$('.feedBack').on('click', '.nextButton', function() {
-			$('.submitButton').unbind('click');
-			$('#correct').unbind('click');
-			$('.incorrect').unbind('click');
-			$('.option').removeAttr('id', 'correct').removeClass('selected');
-			$('.feedBackText').hide();
-			$('.nextButton').hide();
-			questionCounter++;
-			movingOn(); 
-		});
+  }
 
 
-		//after last question
-		else if (questionCounter>5){ 
-			function endCount(){
-				//show final count, give replay option
-				answerCounter=document.getElementById('correctCount').innerText;
-				console.log(document.getElementById('correctCount').innerText);
-				$('.feedBackText').show().text("You got" +answerCounter+ " of 5 correct!");
-				$('.submitButton').hide();
-				$('.replay').show(); 
-				$('.counter').removeClass('visible').addClass('invisible');
-			};
+  //functions!
 
-			endCount(); 
-		}
-	};
+  function newQuestion(question){
+    //default options to incorrect
+    $('.option').addClass('incorrect').css('cursor', 'pointer');
 
-	//replay quiz
-	$('.replay').click(function(){
-		//reset quiz
-		correct=false;
-		questionCounter=1; 
-		movingOn(); 
+    //set question
+    $('.question').text(question.quote);
 
-		//correct items showing/hidden
-		$('.replay').click(function(){
-			correct=false;
-			questionCounter=1;
-			movingOn(); 
-		$('.replay').hide();
-		$('.answer').removeAttr('src', answerClicked).removeClass('fade').siblings().addClass('hidden');
-		$('.counter').removeClass('invisible').addClass('visible');
-		answerCounter= document.getElementById('count');
-		correctCount.innerHTML=0;
-		}
-	});
+    //set options
+    $('.option.1').attr('src', question.options.option1);
+    $('.option.2').attr('src', question.options.option2);
+    $('.option.3').attr('src', question.options.option3);
+    $('.option.4').attr('src', question.options.option4);
 
-		//running the quiz
-		correct=false;
-		questionCounter = 1; 
-		movingOn(); 
+    //add correct flag to right answer
+    $('.option.' + question.answer).attr('id', 'correct').removeClass('incorrect'); 
 
-			
+  };
 
-});
+  //evaluate if correct answer is selected
+  function evaluateGuess(question) {
+    var submitted=false; 
+    //correct answers
+    $('#correct').bind('click', function() {
+      if (submitted == false){
+        $('.submitButton').show();
+        correct=true;
+        $('.option').removeClass('selected'); 
+        $(this).addClass('selected'); 
+
+      };
+
+    });
+
+    //incorrect answers
+    $('.incorrect').bind('click', function(){ 
+      if(submitted==false){
+        $('.submitButton').show(); 
+        correct = false;
+        $('.option').removeClass('selected'); 
+        $(this).addClass('selected'); 
+      };
+    });
+
+    //more with selectedd item
+    $('.submitButton').click(function(){
+      submitted=true;
+      answerClicked = document.getElementsByClassName('selected')[0].getAttribute('src');
+      //correct answer
+      if (correct==true) {
+        answerCounter = document.getElementById('correctCount'); 
+        correctCount.innerHTML++; 
+        $('.feedBackText').show().text("Correct!"); 
+        $('.nextButton').show(); 
+        $('.submitButton').hide(); 
+      }
+
+      //incorrect answer
+      else if (correct==false) {
+        $('.feedBackText').show().text("Incorrect!"); 
+        $('.nextButton').show(); 
+        $('.submitButton').hide(); 
+      }
+
+    });
+
+    //for all the quesiton within the quiz do this
+    $('.feedBack').on('click', '.nextButton', function() {
+      $('.submitButton').unbind('click');
+      $('#correct').unbind('click');
+      $('.incorrect').unbind('click');
+      $('.option').removeAttr('id', 'correct').removeClass('selected');
+      $('.feedBackText').hide();
+      $('.nextButton').hide();
+      questionCounter++;
+      movingOn(); 
+    });
+
+
+    //after last question
+    if (questionCounter>5){ 
+      function endCount(){
+        //show final count, give replay option
+        answerCounter=document.getElementById('correctCount').innerText;
+        console.log(document.getElementById('correctCount').innerText);
+        $('.feedBackText').show().text("You got" +answerCounter+ " of 5 correct!");
+        $('.submitButton').hide();
+        $('.replay').show(); 
+        $('.counter').removeClass('visible').addClass('invisible');
+      };
+
+      endCount(); 
+    }
+  };
+
+  //replay quiz
+  $('.replay').click(function(){
+    //reset quiz
+    correct=false;
+    questionCounter=1; 
+    movingOn(); 
+
+    //correct items showing/hidden
+    $('.replay').click(function(){
+      correct=false;
+      questionCounter=1;
+      movingOn(); 
+      $('.replay').hide();
+      $('.answer').removeAttr('src', answerClicked).removeClass('fade').siblings().addClass('hidden');
+      $('.counter').removeClass('invisible').addClass('visible');
+      answerCounter= document.getElementById('count');
+      correctCount.innerHTML=0;
+    });
+  });
+
+  //running the quiz
+  correct=false;
+  questionCounter = 1; 
+  movingOn(); 
+}();
